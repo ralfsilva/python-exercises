6da2bac20d5b14f260c7c82d7569326acb369d72
Ralf Velane <ralf.velani@gmail.com>
2022-04-19 13:53:38 -0300

    Atualização exercícios Repetição

---
+++ Repeticao/repeticao-18.py
@@ -0,0 +1,15 @@
+count = 1
+ask = True
+x = 0
+
+while ask:
+    if maior > mais:
+        
+
+    num = int(input("Digite um número N: "))
+    
+    maior = num
+
+    if x > maior:
+        x = maior
+    
